f46602bf979bdd3658cb1e47bb1aad8b942c7e87
Pete Gadomski <pete.gadomski@gmail.com>
2022-05-20 14:12:21 -0600

    docs: add docs for async

--- async/src/lib.rs
+++ async/src/lib.rs
@@ -1,3 +1,7 @@
+//! Asynchronous [STAC](https://stacspec.org/).
+
+#![deny(missing_docs, missing_debug_implementations)]
+
 mod read;
 
 pub use read::{AsyncRead, AsyncReader};
--- async/src/read.rs
+++ async/src/read.rs
@@ -6,8 +6,22 @@ use stac::{Error, Href, HrefObject, Object, Result};
 use std::path::{Path, PathBuf};
 use url::Url;
 
+/// Reads STAC objects asynchronously.
 #[async_trait]
 pub trait AsyncRead {
+    /// Reads a [HrefObject] asynchronously.
+    ///
+    /// # Examples
+    ///
+    /// [AsyncReader] implements [AsyncRead]:
+    ///
+    /// ```
+    /// use stac_async::{AsyncRead, AsyncReader};
+    /// let reader = AsyncReader::new();
+    /// # tokio_test::block_on(async {
+    /// let catalog = reader.read("data/catalog.json").await.unwrap();
+    /// # })
+    /// ```
     async fn read<H>(&self, href: H) -> Result<HrefObject>
     where
         H: Into<Href> + Send + Sync,
@@ -21,6 +35,7 @@ pub trait AsyncRead {
         })
     }
 
+    /// Reads json data asynchonously.
     async fn read_json<H>(&self, href: H) -> Result<Value>
     where
         H: Into<Href> + Send + Sync,
@@ -31,22 +46,30 @@ pub trait AsyncRead {
         }
     }
 
+    /// Reads json data from a local filesystem path.
     async fn read_json_from_path<P>(&self, path: P) -> Result<Value>
     where
         P: AsRef<Path> + Send + Sync;
+
+    /// Reads json data from a remote url.
     async fn read_json_from_url(&self, url: Url) -> Result<Value>;
 }
 
+/// A simple structure for reading STAC data asynchronously.
+///
+/// Includes a [Client] which is used to pool requests.
 #[derive(Debug, Default)]
 pub struct AsyncReader {
     client: Client,
 }
 
 impl AsyncReader {
+    /// Cretaes a new AsyncReader.
     pub fn new() -> AsyncReader {
         AsyncReader::default()
     }
 
+    /// Returns a (cheap) clone of this reader's [Client].
     pub fn client(&self) -> Client {
         self.client.clone()
     }
